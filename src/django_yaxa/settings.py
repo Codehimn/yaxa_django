2a14f996abfe684f3c168361404586d22765c4cd
Codehimn <Gabor285@gmail.com>
2017-01-02 22:21:58 -0500

    .

--- src/django_yaxa/settings.py
+++ src/django_yaxa/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'django_yaxa.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'yaxaws',
+        'USER': 'root',
+        'PASSWORD': 'abc123456',
+        'HOST': 'yaxabot-buyer.ddns.net',
+        'PORT': '3311',
     }
 }
 
