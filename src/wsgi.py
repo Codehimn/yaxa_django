90a7d456db16ef9c7438c0e6801eeb3fdc5f9ff9
Julian Acosta <julian.acosta@yaxa.co>
2017-01-05 10:45:18 -0500

    Start

--- src/wsgi.py
+++ src/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_yaxa project.
+WSGI config for yaxa_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
 
 application = get_wsgi_application()
